acf0a3ab9c5ce4e16ba154b785b3480b221768c7
sinnkami <sinnkami.suraidaku@gmail.com>
2017-03-09 16:27:58 +0900

    herokuのサイトによればこれで動くっぽい？

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 const pg = require('pg');
+pg.defaults.ssl = true;
 
-const connectionString = "tcp://localhost:5432/rougelike::DATABASE";
+//const connectionString = "tcp://localhost:5432/game_rouge";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,7 +16,7 @@ router.get('/game', function (req, res) {
 })
 
 router.post(`/score`, function (req, res) {
-  pg.connect(connectionString, (error, client) => {
+  pg.connect(process.env.DATABASE_URL, (error, client) => {
     if (error) { console.error(error); }
     var query = client.query('INSERT INTO score(game_name, score, lv, maxhp, attack, defense) VALUES($1,$2,$3,$4,$5,$6)',
     [req.body.name, req.body.score, req.body.level, req.body.hp, req.body.attack, req.body.defense],
