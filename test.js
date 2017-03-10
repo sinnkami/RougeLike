e92395731cd6903c5c92b26f175c7d8ae7888775
sinnkami <sinnkami.suraidaku@gmail.com>
2017-03-10 14:22:00 +0900

    db.jsを修正した

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const pg = require('pg');
 
-const connectionString = "tcp://localhost:5432/game_rouge";
+if (process.env.DATABASE_URL){
+  pg.defaults.ssl = true;
+}
+
+const connectionString = process.env.DATABASE_URL || "tcp://localhost:5432/game_rouge";
 
 pg.connect(connectionString, function(error, client) {
   var query = client.query('INSERT INTO score(game_name, score, lv, maxhp, attack, defense) VALUES($1,$2,$3,$4,$5,$6)',["sinnkami", 1000000, 100, 500, 50, 30], function (err, result) {
