a04f2a044a0061d963f552aae5171f69ffe88a08
sinnkami <sinnkami.suraidaku@gmail.com>
2017-03-10 15:40:34 +0900

    名無しだと進めないようにした

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,6 +4,9 @@ $(function () {
 
   start.click(function () {
     var set = encodeURI(name.value);
+    if (set.length == 0){
+      return false;
+    }
     location.href = `/game?name=${set}`
   })
 })
--- test_data.js
+++ test_data.js
@@ -1,8 +1,8 @@
-GameManager.game.player.status.name = generateRandomAlphaNum(Math.floor(Math.random()*10));
+GameManager.game.player.status.name = "sinnkami";
 GameManager.game.player.status.maxhp = Math.floor(Math.random()*999);
 GameManager.game.player.status.attack = Math.floor(Math.random()*999);
 GameManager.game.player.status.defense = Math.floor(Math.random()*999);
-GameManager.game.player.status.level = Math.floor(Math.random() * 10);
+GameManager.game.player.status.level = Math.floor(Math.random() * 100);
 GameManager.game.player.point = Math.floor(Math.random()*1000000000);
 
 GameManager.game.player.status.hp = 0;
